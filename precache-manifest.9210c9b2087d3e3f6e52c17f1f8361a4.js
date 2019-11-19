e1994deda0b9ce7a01a8d3d933c9eafe6313d30c
Vladyslav Mykhailenko <v.mykhailenko@atomcream.com>
2019-11-19 05:48:29 +0200

    Updates

---
+++ precache-manifest.9210c9b2087d3e3f6e52c17f1f8361a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc7ce5c65269ee75b7d40d22e17c946",
+    "url": "/Vlad-Mykhailenko-17-11-2019/index.html"
+  },
+  {
+    "revision": "dab131bef734498daf14",
+    "url": "/Vlad-Mykhailenko-17-11-2019/static/css/main.75b098f6.chunk.css"
+  },
+  {
+    "revision": "1636cef2d471e2aa0944",
+    "url": "/Vlad-Mykhailenko-17-11-2019/static/js/2.09ab01bf.chunk.js"
+  },
+  {
+    "revision": "dab131bef734498daf14",
+    "url": "/Vlad-Mykhailenko-17-11-2019/static/js/main.a1a21a01.chunk.js"
+  },
+  {
+    "revision": "5727e627fbe9dd0c22e0",
+    "url": "/Vlad-Mykhailenko-17-11-2019/static/js/runtime-main.2b4a3fba.js"
+  },
+  {
+    "revision": "973322d65f10e6b9e2906015b046bc52",
+    "url": "/Vlad-Mykhailenko-17-11-2019/static/media/weather.973322d6.jpg"
+  }
+]);
